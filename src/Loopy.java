dc9108af9e1ed6444780e525ec791d43c9c0e577
Axel_ST <kozlov.axe@gmail.com>
2021-03-18 13:09:21 +0700

    Edit Loopy class
    Head First Java, 2nd Edition (RU) p. 42

--- src/Loopy.java
+++ src/Loopy.java
@@ -1,6 +1,18 @@
 /*
-* Created by Axel_ST on 18.03.2021
-*/
+ * Created by Axel_ST on 18.03.2021
+ *
+ * Head First Java, 2nd Edition (RU) p. 42
+ */
 
-package PACKAGE_NAME;public class Loopy {
+public class Loopy {
+   public static void main(String[] args) {
+      int x = 1;
+      System.out.println("Перед циклом");
+      while (x < 4) {
+         System.out.println("В цикле");
+         System.out.println("Значение х = " + x);
+         x++;
+      }
+      System.out.println("После цикла");
+   }
 }
