5bd8ff5d51b74eb08013380ce251cb6d0596efc9
Axel_ST <kozlov.axe@gmail.com>
2021-03-18 14:06:21 +0700

    Edit Shuffle1 class
    Head First Java, 2nd Edition (RU) p. 50 ex. Magnets with code

--- src/Shuffle1.java
+++ src/Shuffle1.java
@@ -1,6 +1,34 @@
 /*
-* Created by Axel_ST on 18.03.2021
-*/
+ * Created by Axel_ST on 18.03.2021
+ *
+ * Head First Java, 2nd Edition (RU) p. 50 ex. Magnets with code
+ */
 
-package PACKAGE_NAME;public class Shuffle1 {
+public class Shuffle1 {
+   public static void main(String[] args) {
+      
+      int x = 3;
+      
+      while (x > 0) {
+         
+         if (x > 2) {
+            System.out.print("a");
+         }
+         
+         x--;
+         System.out.print("-");
+         
+         if (x == 2) {
+            System.out.print("b c");
+         }
+         
+         x--;
+         System.out.print("-");
+         
+         if (x == 1) {
+            System.out.print("d");
+            x--;
+         }
+      }
+   }
 }
