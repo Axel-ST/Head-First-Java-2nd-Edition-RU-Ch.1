4e6c5be3ec718ec8097dc9f83e64559335b67043
Axel_ST <kozlov.axe@gmail.com>
2021-03-18 13:17:49 +0700

    Edit IfTest12 class
    Head First Java, 2nd Edition (RU) p. 43
    ifTest & IfTest2 combines

--- src/IfTest12.java
+++ src/IfTest12.java
@@ -1,6 +1,23 @@
 /*
-* Created by Axel_ST on 18.03.2021
-*/
+ * Created by Axel_ST on 18.03.2021
+ *
+ * Head First Java, 2nd Edition (RU) p. 43
+ */
 
-package PACKAGE_NAME;public class IfTest12 {
+public class IfTest12 {
+   public static void main(String[] args) {
+      int x = 3;
+      
+      if (x == 3)
+         System.out.println("х равно 3");
+      System.out.println("Напишет в любом случае");
+      
+      x = 2;
+      
+      if (x == 3)
+         System.out.println("х равно 3");
+      else
+         System.out.println("х не равно 3");
+      System.out.println("Напишет в любом случае");
+   }
 }
