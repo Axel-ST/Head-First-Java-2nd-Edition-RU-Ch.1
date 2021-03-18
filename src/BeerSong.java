f1d2d788f9f5f9c1b0498f7acd09ad50c93d28b3
Axel_ST <kozlov.axe@gmail.com>
2021-03-18 14:34:44 +0700

    Corrected some classes files
    Format code with a single codestyle

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -10,12 +10,12 @@ public class BeerSong {
    public static void main(String[] args) {
       int beerNum = 99;
       String word = "бутылок (бутылки)";
-         System.out.println(beerNum + " " + word + " пива на столе");
+      System.out.println(beerNum + " " + word + " пива на столе");
       
       while (beerNum > 0) {
          if (beerNum == 1)
             word = "бутылка"; // на последний прогон, одна бутылка
-   
+         
          System.out.println(beerNum + " " + word + " пива на столе");
          System.out.println(beerNum + " " + word + " пива");
          System.out.println("Возьми одну, пусти по кругу");
