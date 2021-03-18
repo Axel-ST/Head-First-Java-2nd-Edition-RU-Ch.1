b29d215b63cc4125ede93bfc92be8096eb90ca9c
Axel_ST <kozlov.axe@gmail.com>
2021-03-18 13:05:54 +0700

    Edit MyFirstApp class
    Head First Java, 2nd Edition (RU) p. 39

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -1,6 +1,12 @@
 /*
-* Created by Axel_ST on 18.03.2021
-*/
+ * Created by Axel_ST on 18.03.2021
+ *
+ * Head First Java, 2nd Edition (RU) p. 39
+ */
 
-package PACKAGE_NAME;public class MyFirstApp {
+public class MyFirstApp {
+   public static void main(String[] args) {
+      System.out.println("Я управляю");
+      System.out.println("миром!");
+   }
 }
