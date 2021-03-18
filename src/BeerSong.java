272037ef3f6b5eef8635c531bceb138eacadfbfc
Axel_ST <kozlov.axe@gmail.com>
2021-03-18 13:37:12 +0700

    Edit BeerSong class
    Head First Java, 2nd Edition (RU) p. 44

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,6 +1,27 @@
 /*
-* Created by Axel_ST on 18.03.2021
-*/
+ * Created by Axel_ST on 18.03.2021
+ *
+ * Head First Java, 2nd Edition (RU) p. 44
+ *
+ * My version of this exercise
+ */
 
-package PACKAGE_NAME;public class BeerSong {
+public class BeerSong {
+   public static void main(String[] args) {
+      int beerNum = 99;
+      String word = "бутылок (бутылки)";
+         System.out.println(beerNum + " " + word + " пива на столе");
+      
+      while (beerNum > 0) {
+         if (beerNum == 1)
+            word = "бутылка"; // на последний прогон, одна бутылка
+   
+         System.out.println(beerNum + " " + word + " пива на столе");
+         System.out.println(beerNum + " " + word + " пива");
+         System.out.println("Возьми одну, пусти по кругу");
+         System.out.println();
+         beerNum--;
+      }
+      System.out.println("Закончилось..");
+   }
 }
